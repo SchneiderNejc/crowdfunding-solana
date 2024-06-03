c19d4f2bedc7641f309ea59edcab47aab03f4fb6
Nejc Schneider <nejc.sch@gmail.com>
2024-06-03 18:57:06 +0200

    add struct Withdraw

--- programs/crowdfunding-solana/src/lib.rs
+++ programs/crowdfunding-solana/src/lib.rs
@@ -46,6 +46,14 @@ pub struct Create<'info> {
     pub system_program: Program<'info, System>
 }
 
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>
+}
+
+
 #[account]
 pub struct Campaign {
     pub admin: Pubkey,
