003f97a7a59cfcbedbca38318618c3be74b6f9e5
Nejc Schneider <nejc.sch@gmail.com>
2024-06-04 21:03:17 +0200

    update project id

--- programs/crowdfunding-solana/src/lib.rs
+++ programs/crowdfunding-solana/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("9DS9Yz9FxwSStWXzFqUePuu46aEZ5xrYbwiSoxcZgkZm");
 
 #[program]
 pub mod crowdfunding_solana {
