16af4328f5130d9b6d9d518de912145dc4ccd775
Nejc Schneider <nejc.sch@gmail.com>
2024-06-04 21:07:01 +0200

    airdrop

--- README.ts
+++ README.ts
@@ -2,3 +2,5 @@ solana-keygen new -o id.json
 -create a new wallet for local project
 -don't forget to copy the pubkey
 
+solana airdrop 2 CUTFj7GMje9fyBY5AjWZNDmwLoEZSQ3PJnSP3T7HyeEp --url devnet
+-airdrop to the selected address
