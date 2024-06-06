676b127bd9f291178a80f34f091a2fe82653d79c
Nejc Schneider <nejc.sch@gmail.com>
2024-06-06 11:11:28 +0200

    check if Phantom wallet is connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 const App = () => {
   const checkIfWalletIsConnected = async() => {
     try {
+
+      const {solana} = window;
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found!");
+        } else {
+          alert("Solana object not found! Get a Phantom wallet");
+        }
+      }
     } catch(error) {
       console.error(error)
     }
