0bba82f10b75da9ed5652cf125ae28dfa51ec9fe
Nejc Schneider <nejc.sch@gmail.com>
2024-06-06 11:10:31 +0200

    recreate App()

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
 
+const App = () => {
+  const checkIfWalletIsConnected = async() => {
+    try {
+    } catch(error) {
+      console.error(error)
+    }
+  };
 }
 
 export default App;
