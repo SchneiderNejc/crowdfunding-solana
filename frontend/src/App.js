81e94b3a7eb359cf43d813ad84501b02c939c9b3
Nejc Schneider <nejc.sch@gmail.com>
2024-06-06 12:46:23 +0200

    move wallet connection to connectWallet button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,14 @@ const App = () => {
   };
 
 
-const connectWallet = async() => {};
+const connectWallet = async() => {
+  const {solana} = window;
+  if (solana) {
+    const response = await solana.connect()
+    console.log('Connected with public key:', response.publicKey.toString());
+    setWalletAddress(response.publicKey.toString());
+  }
+};
 
 const renderNotConnectedContainer = () => (
   <button onClick={connectWallet}>Connect to Wallet</button>
